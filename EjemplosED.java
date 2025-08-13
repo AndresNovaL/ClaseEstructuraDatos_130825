691288bbc92abb7a6710281e5a60f1cf207a603b
JovenDan <>
2025-08-13 17:12:51 -0500

    Ejemplos de las estructuras vistas en clase.

---
+++ EjemplosED.java
@@ -0,0 +1,39 @@
+public class EjemplosED {
+    public static void main(String[] args) {
+
+        // Estructura de Datos tipo Int
+        int años_edad = 19;
+        int año = 2025;
+        int dias_semana = 7;
+        int manzanas = 12;
+        int cantidad_pisos = 20;
+
+        // Estructura de Datos tipo Double
+        double tiempo = 12.5;
+        double temperatura = 36.6;
+        double distancia = 12.5;
+        double velocidad = 89.5;
+        double masa = 72.3;
+
+        // Estructura de Datos tipo Char
+        char a = 'J';
+        char b = 'U';
+        char c = 'L';
+        char d = 'I';
+        char e = 'O';
+
+        // Estructura de Datos tipo String
+        String saludo = "¡Bievenido a la clase!";
+        String despedida = "¡Hasta pronto!";
+        String marca = "Mercedes Benz";
+        String computador = "ASUS";
+        String mascota = "Gato";
+
+        // Estructura de Datos tipo Boolean
+        Boolean dia = false;
+        Boolean noche = true;
+        Boolean vivo = true;
+        Boolean muerto = false;
+        Boolean hombre = true;
+    }
+}
---
+++ EjemplosED.py
@@ -0,0 +1,34 @@
+## Estructura de Datos Int
+estudiantes = 25
+cantidad_teclas = 105
+dias_semana = 7
+cantidad_horas = 24
+edad = 17
+
+## Estructura de Datos Double
+tiempo = 15.4
+temperatura = 37.8
+distancia = 29.62
+velocidad = 120.9
+masa = 49.6
+
+## Estructura de Datos Char
+a = 'M'
+b = 'O'
+c = 'U'
+d = 'S'
+e = 'E'
+
+## Estructura de Datos tipo String
+deporte = "Tenis de campo"
+universidad = "Universidad de San Buenaventura"
+marca = "New Balance"
+pais = "Suiza"
+procesador = "Intel Core"
+
+## Estructura de Datos tipo Boolean
+color_naranja = True;
+alienigena = False;
+mayorEdad = True;
+dipositivoEncendido = False;
+contraseñaValida = True;
