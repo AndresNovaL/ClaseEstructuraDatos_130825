597972ffed1cb33665292796daf9ce12d41acd98
JovenDan <>
2025-08-13 16:13:35 -0500

    Las cuatro operaciones básicas terminadas

--- EstructurasEstaticas.java
+++ EstructurasEstaticas.java
@@ -6,12 +6,20 @@ public class EstructurasEstaticas {
         int a = 10;
         int b = 11;
 
-        // int c = 10, d = 11;
+        // int c = 10, d = 11
 
         int suma = a + b;
         int resta = a - b;
+        double multiplicacion = a * b;
+        // Optional Division = a / b --> Optinal no es un tipo de dato. No genera error.
+        // Puede ser utilizado en divisiones por cero.
+        double division = a / b;
+
+        // long c = 30000000000000L
 
         System.out.println("La suma es: " + suma);
         System.out.println("La resta es: " + resta);
+        System.out.println("La multiplicación es: " + multiplicacion);
+        System.out.println("La división es: " + division);
     }
 }
--- EstructurasEstaticas.py
+++ EstructurasEstaticas.py
@@ -7,3 +7,10 @@ b = 11
 
 suma = a + b
 resta = a - b
+multiplicacion = a * b
+division = a / b
+
+print("La suma es: ", suma)
+print("La resta es: ", resta)
+print("La multiplicación es: ", multiplicacion)
+print("La división es: ", division)
